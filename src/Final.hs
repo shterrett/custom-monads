d8ef8d5bc2e583f46bf5f4bab5843505d20ff85e
Stuart Terrett <shterrett@gmail.com>
2020-02-05 21:12:47 -0500

    ormolu formatting

--- src/Final.hs
+++ src/Final.hs
@@ -6,41 +6,41 @@ import qualified Data.Text as T
 import Utils
 
 class Hangman m where
-    getInput :: m Text
-    showOutput :: Text -> m ()
-    targetWord :: m Target
-    correctGuesses :: m (Set Char)
-    incorrectGuesses :: m (Set Char)
-    putCorrect :: Char -> m ()
-    putIncorrect :: Char -> m ()
-    threshold :: m Int
+  getInput :: m Text
+  showOutput :: Text -> m ()
+  targetWord :: m Target
+  correctGuesses :: m (Set Char)
+  incorrectGuesses :: m (Set Char)
+  putCorrect :: Char -> m ()
+  putIncorrect :: Char -> m ()
+  threshold :: m Int
 
 turn :: (Monad m, Hangman m) => m GuessResult
 turn = do
-    t <- targetWord
-    correct <- correctGuesses
-    showOutput $ showTarget t correct
-    incorrect <- incorrectGuesses
-    showOutput $ showIncorrect incorrect
-    guess <- showOutput "Next Guess: " >> getInput
-    let guessResult = isChar guess >>= alreadyGuessed correct incorrect >>= checkGuess t
-    case guessResult of
-      Right c -> continueCorrect c
-      Left err -> continueIncorrect err
+  t <- targetWord
+  correct <- correctGuesses
+  showOutput $ showTarget t correct
+  incorrect <- incorrectGuesses
+  showOutput $ showIncorrect incorrect
+  guess <- showOutput "Next Guess: " >> getInput
+  let guessResult = isChar guess >>= alreadyGuessed correct incorrect >>= checkGuess t
+  case guessResult of
+    Right c -> continueCorrect c
+    Left err -> continueIncorrect err
 
 continueCorrect :: (Monad m, Hangman m) => Char -> m GuessResult
 continueCorrect c = do
-    putCorrect c
-    winGame <$> targetWord <*> correctGuesses
+  putCorrect c
+  winGame <$> targetWord <*> correctGuesses
 
 continueIncorrect :: (Monad m, Hangman m) => GuessError -> m GuessResult
 continueIncorrect err = do
-    case err of
-      Incorrect c -> do
-        putIncorrect c
-        showOutput $ T.singleton c <> " is not in the word"
-      AlreadyGuessed c -> do
-        showOutput $ T.singleton c <> " has already been guessed"
-      NotChar -> do
-        showOutput $ "Please enter a character between a and z"
-    loseGame <$> incorrectGuesses
+  case err of
+    Incorrect c -> do
+      putIncorrect c
+      showOutput $ T.singleton c <> " is not in the word"
+    AlreadyGuessed c ->
+      showOutput $ T.singleton c <> " has already been guessed"
+    NotChar ->
+      showOutput "Please enter a character between a and z"
+  loseGame <$> incorrectGuesses
--- src/Utils.hs
+++ src/Utils.hs
@@ -7,69 +7,76 @@ import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
 
-newtype Target = Target { unTarget :: Text }
+newtype Target = Target {unTarget :: Text}
 
-data GuessError =
-    Incorrect Char
-    | AlreadyGuessed Char
-    | NotChar
-    deriving (Eq, Show)
+data GuessError
+  = Incorrect Char
+  | AlreadyGuessed Char
+  | NotChar
+  deriving (Eq, Show)
 
-data GuessResult =
-    Continue
-    | Win
-    | Lose
-    deriving (Show, Eq)
+data GuessResult
+  = Continue
+  | Win
+  | Lose
+  deriving (Show, Eq)
 
 showTarget :: Target -> Set Char -> Text
 showTarget (Target t) correct =
-    mconcat $ obfuscate <$> T.unpack t
-  where obfuscate c = if Set.member c correct
-                        then T.singleton c
-                        else unknown
+  mconcat $ obfuscate <$> T.unpack t
+  where
+    obfuscate c =
+      if Set.member c correct
+        then T.singleton c
+        else unknown
 
 showIncorrect :: Set Char -> Text
 showIncorrect incorrect =
-    "Previous Guesses: " <> guesses
-  where guesses = T.intersperse ' ' (T.pack $ sort $ Set.elems incorrect)
+  "Previous Guesses: " <> guesses
+  where
+    guesses = T.intersperse ' ' (T.pack $ sort $ Set.elems incorrect)
 
 unknown :: Text
 unknown = "▮"
 
 isChar :: Text -> Either GuessError Char
 isChar = toEither . T.unpack
-  where toEither = \case
-                     c:"" | isAlpha c -> Right $ toLower c
-                          | otherwise -> Left NotChar
-                     _ -> Left NotChar
+  where
+    toEither = \case
+      c : ""
+        | isAlpha c -> Right $ toLower c
+        | otherwise -> Left NotChar
+      _ -> Left NotChar
 
 alreadyGuessed ::
-  Set Char
-  -> Set Char
-  -> Char
-  -> Either GuessError Char
+  Set Char ->
+  Set Char ->
+  Char ->
+  Either GuessError Char
 alreadyGuessed correct incorrect c =
-    if Set.member c (Set.union correct incorrect)
-      then Left $ AlreadyGuessed c
-      else Right c
+  if Set.member c (Set.union correct incorrect)
+    then Left $ AlreadyGuessed c
+    else Right c
 
 checkGuess :: Target -> Char -> Either GuessError Char
 checkGuess (Target t) c =
-    if T.any (== c) t
-      then Right c
-      else Left $ Incorrect c
+  if T.any (== c) t
+    then Right c
+    else Left $ Incorrect c
 
 loseGame :: Set Char -> GuessResult
 loseGame incorrect =
-    if Set.size incorrect >= maxMisses
-      then Lose
-      else Continue
+  if Set.size incorrect >= maxMisses
+    then Lose
+    else Continue
 
 winGame :: Target -> Set Char -> GuessResult
-winGame (Target t) correct = if correct == haystack
-              then Win
-              else Continue
-  where haystack = Set.fromList $ T.unpack t
+winGame (Target t) correct =
+  if correct == haystack
+    then Win
+    else Continue
+  where
+    haystack = Set.fromList $ T.unpack t
 
 maxMisses :: Int
 maxMisses = 5
